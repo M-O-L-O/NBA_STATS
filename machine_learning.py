036589d7ae62f7cb57ed1510a14ea6169f6d9aaa
M-O-L-O <78034469+M-O-L-O@users.noreply.github.com>
2025-03-23 19:23:59 +0000

    added z-score for distance

--- machine_learning.py
+++ machine_learning.py
@@ -23,21 +23,38 @@ from torchvision import transforms, utils
 # Set PyTorch print settings
 torch.set_printoptions(threshold=10000, linewidth=140, precision=2)
 
+# Standard Deviation ignoring NaN values
+def nanstd(o, dim, keepdim=False):
+    result = torch.sqrt(
+                torch.nanmean(
+                    torch.pow( torch.abs(o-torch.nanmean(o,dim=dim).unsqueeze(dim)),2),
+                    dim=dim
+                )
+            )
+    
+    if keepdim:
+        result = result.unsqueeze(dim)
+        
+    return result
+
 # Get each players stats - historical data from somewhere (unchanging so save locally)
 data = pd.read_csv('data/allPlayerStatsNew.csv')
 
+headers = ['PTS','MP','AST','STL','BLK','TRB','TOV','FG','FGA','FG%','3P','3PA','3P%','2P','2PA','2P%','FT','FTA','FT%','eFG%','ORB','DRB','PF',]
 
-def dataFormat(raw_data, player_name):
+len_tensor = len(headers)
+
+def dataFormat(raw_data, player_name, headers = ['PTS','MP','AST','STL','BLK','TRB','TOV','FG','FGA','FG%','3P','3PA','3P%','2P','2PA','2P%','FT','FTA','FT%','eFG%','ORB','DRB','PF',]
+    ):
     
     # Inputs Dataframe and outputs a tensor with data on player X formatted how we want (years combined and useless stuff stripped)
-
+    # Normalise first??
+    
     player_data = raw_data.loc[raw_data['Name'] == player_name]
     
     years_played = np.unique(player_data['Year'])
     
     formatted_data = []
-    
-    headers = ['PTS','MP','AST','STL','BLK','TRB','TOV','FG','FGA','FG%','3P','3PA','3P%','2P','2PA','2P%','FT','FTA','FT%','eFG%','ORB','DRB','PF',]
     
     for i, year in enumerate(years_played):
         interm = player_data.loc[player_data['Year'] == year][headers]
@@ -53,7 +70,7 @@ def dataFormat(raw_data, player_name):
         
         else:
             #print(raw_player_year_x.size(), games_proportion.size())
-            multipl = torch.matmul(torch.transpose(raw_player_year_x, 0, 1), games_proportion.T)
+            multipl = torch.matmul(torch.transpose(raw_player_year_x, 0, 1), games_proportion.t())
             games_section = torch.tensor([float(year), float(games_played),float(games_started)])
         
         # Should all condense into one line
@@ -77,15 +94,32 @@ def allDataFormat(raw_data):
     for player in player_names:
         X.append(dataFormat(data, player))
     
-    return X
+    # After got all data correct, normalise
+    X_out = []
+    #    Return normalised distance using Z score (SHOULD THIS BE A PRECURSORY STEP BEFORE CALCULATING DISTANCE??? - NORMALISE STATS THEN CALCULATE DISTANCE??)
+    #    RIGHT NOW IT'LL NORMALISE THE DISTANCE BETWEEN EACH STAT
+
+    for x in X: 
+        columns = torch.transpose(torch.cat(x),0,1)
+        # - you want to get rid of column index 0 as it is year, which shouldn't be normalised
+        # should the games played be normalised? yes as it doesnt make a big diff
+        # set mean to 0 and standard deviations to 1?
+        means = torch.tensor([torch.nanmean(column, 0).item() for column in columns])
+        stds = torch.tensor([nanstd(column, 0).item() for column in columns])
+        means[0] = torch.zeros(len(means[0]))
+        stds[0] = torch.ones(len(stds[0]))
+        
+        
+        for datum in x:
+            X_out.append(torch.div( (datum - means.repeat(len(datum), 1) ), stds.repeat(len(datum), 1) ))
+    
+    return X_out
 
 # Just realised name not needed - can exclude
 
 X = allDataFormat(data)
 
-print(X[5])
-print(X[50])
-print(X[150])
+#print(torch.cat(X).size())
 
 ### DBSCAN Algorithm
 
@@ -95,36 +129,42 @@ print(X[150])
 
 # 2) Compute core distance
 
-# Make distance function
+#    Make distance function
 def dist(player1=torch.Tensor, player2=torch.Tensor):
     # find which has highest years
     
-    # Using matrices of player data - pull column of years and use to replace len
+    # Using matrices of player data - pull column of years and use to replace len -- len works as one by one years
     year1, year2 = len(player1), len(player2)
     
     min_it = int(np.min([year1,year2]))
     
-    # Here add rows instead of expand_as (unless expand_as adds a row)
+    
+    # Here calculate dist between same shape parts then append the mean to fill the end
+    
     if year1 < year2:
-        player1.expand_as(player2)
+        # Player 1 less years
+        dist_half = torch.abs(torch.subtract(player1,player2[:min_it]))
+        dist_vec = torch.cat( (dist_half, (1.5 * torch.nanmean(dist_half, 0)).repeat(year2 - year1,1)) )
     elif year1 > year2:
-        player2.expand_as(player2)
+        # Player 2 less years
+        dist_half = torch.abs(torch.subtract(player2,player1[:min_it]))
+        dist_vec = torch.cat( (dist_half, (1.5 * torch.nanmean(dist_half, 0)).repeat(year1 - year2,1)) )
+    else:
+        dist_vec = torch.abs(torch.subtract(player1,player2))
         
-    # Subtract matrices from each other to get distances
-    dist_vec = torch.abs(torch.subtract(player1,player2))
-    
-    # If the years dont match, fill gap rows with mean multipliers
-    try:    
-        dist_vec[:min_it] = torch.torch.mean(dist_vec[min_it:], 1) * 1.5
-    except:
-        pass
-    
-    
-    
-    # for every year, find the distance between each player in each stat category and record
-    
-    dist_vec = (player1 - player2)
+    return dist_vec
 
+print(dist(X))
+
+def DBSCAN():
+    
+    pass
+
+def RangeQuery():
+    pass
+
+
+# 3) Use core distance to determine which to group together
 
 # Test different versions: 
 # - career averages with length as stat
@@ -140,3 +180,11 @@ def dist(player1=torch.Tensor, player2=torch.Tensor):
 
 
 
+##### ARCHIVE
+
+#    Determine ranges for normalisation
+def determineAverages(form_data):
+    columns = torch.transpose(torch.cat(form_data),0,1)
+    #return [int(torch.max(column)) for column in columns]
+    returns = [(torch.nanmean(column, 0).item(), nanstd(column, 0).item()) for column in columns]
+    return returns, len(returns)
